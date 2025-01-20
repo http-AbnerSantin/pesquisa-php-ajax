0ab7bb4cc0e9400009de38883d339fa7d341c920
http-AbnerSantin <abner.santin11@gmail.com>
2025-01-20 14:40:53 -0300

    First commit

--- Php/busca.php
+++ Php/busca.php
@@ -2,11 +2,18 @@
 include("connect.php");
 $response = '';
 
-
+if (isset($_POST['filtro'])) {
+    $filtro = $_POST['filtro'];  // Atribui o valor do filtro à variável $filtro
+    echo "Filtro selecionado: " . htmlspecialchars($filtro);  // Exibe o valor selecionado com segurança
+    echo $filtro;
+} else {
+    echo '<script>alert("Nenhum filtro foi selecionado")</script>';
+}
 
 if(isset($_POST['buscar']) && !empty($_POST['buscar'])){
+
     $buscar = $_POST['buscar'];
-    $cmd = $pdo->prepare("SELECT * FROM cidadao where nome LIKE '%$buscar%'");
+    $cmd = $pdo->prepare("SELECT * FROM itens where descricao LIKE '$buscar%'");
     $cmd->execute();
     $dados = $cmd->fetchAll(PDO::FETCH_CLASS);
 
@@ -15,15 +22,50 @@ if(isset($_POST['buscar']) && !empty($_POST['buscar'])){
             $response .= '
 
             <tr>
-                <td>'.$dado->id.'</td>
-                <td>'.$dado->nome.'</td>
-                <td>'.$dado->email.'</td>
-                <td>Maputo</td>
-                <td>2342342342B</td>
-                <td>
-                    <button class="btn-sm btn-outline-primary"><i class="fa-solid fa-edit"></i></button>
-                    <button class="btn-sm btn-outline-danger"><i class="fa-solid fa-trash-can"></i></button>
-                </td>
+                <td>'.$dado->codigo_barras.'</td>
+                <td>'.$dado->descricao.'</td>
+                <td>'.$dado->fornecedor_id.'</td>
+                <td>'.$dado->estoque_pdv3.'</td>
+                <td>'.$dado->estoque_pdv6.'</td>
+                <td>'.$dado->estoque_pdv5.'</td>
+                <td>'.$dado->estoque_pdv7.'</td>
+                <td>'.$dado->estoque_atual.'</td>
+                <td>'.$dado->estoque_pdv1.'</td>
+                <td>'.$dado->estoque_pdv2.'</td>
+                <td>'.$dado->estoque_pdv4.'</td>
+                <td>'.$dado->estoque_pdv9.'</td>
+                <td>'.$dado->estoque_pdv12.'</td>
+                <td>'.$dado->estoque_pdv8.'</td>
+                <td>'.$dado->estoque_pdv10.'</td>
+                <td>'.$dado->estoque_pdv11.'</td>
+                <td>'.$dado->estoque_pdv13.'</td>
+                <td>'.$dado->estoque_apartado.'</td>
+                <td>'.$dado->media_venda.'</td>
+                <td>'.$dado->custo_venda.'</td>
+                <td>'.$dado->ativo.'</td>
+                <td>'.$dado->valor_venda4.'</td>
+                <td>'.$dado->valor_venda3.'</td>
+                <td>'.$dado->valor_venda2.'</td>
+                <td>'.$dado->valor_venda1.'</td>
+                <td>'.$dado->minimo_p.'</td>
+                <td>'.$dado->minimo_d.'</td>
+                <td>'.$dado->e1.'</td>
+                <td>'.$dado->e2.'</td>
+                <td>'.$dado->e3.'</td>
+                <td>'.$dado->e4.'</td>
+                <td>'.$dado->item_id.'</td>
+
+
+
+
+
+
+
+
+
+
+
+
             </tr>
 
             ';
@@ -33,7 +75,7 @@ if(isset($_POST['buscar']) && !empty($_POST['buscar'])){
 
         <tr>
         
-            <td colspan="6">Nenhum Usuario foi achado</td>
+            <td colspan="6">Nenhum item foi achado</td>
         
         </tr>
 
--- Php/connect.php
+++ Php/connect.php
@@ -1,9 +1,9 @@
 <?php
 
-$localhost= 'localhost';
-$dbname= 'crud-modal';
-$usuario= 'root';
-$senha= '';
+$localhost= '15.235.82.182';
+$dbname= 'sysdataw_phoenixweb_caninana';
+$usuario= 'sysdataw_adm';
+$senha= 'wr35234382';
  
 
 $pdo = new PDO("mysql:host=".$localhost.";dbname=".$dbname.";",$usuario,$senha);
